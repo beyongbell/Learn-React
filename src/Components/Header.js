826630e33a572999b33ff2461454186988770648
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-08-31 17:58:09 +0700

    Install React Spring

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navigation from './Navigation'
 
 export default function Header() {
     return (
         <div>
-            <header className="border-b font-bold p-3">
-                AppName
+            <header className="border-b p-3 flex justify-between items-center">
+                <span className="font-bold"> AppName </span> 
+                <Navigation />
             </header>
         </div>
     )
